89e1ffabf707f85110ec49cb36cf4d9141981138
hossainahammed <hossainahammed627@gmail.com>
2025-03-26 20:38:01 +0600

    for_loop

--- lib/for_loop.dart
+++ lib/for_loop.dart
@@ -1,5 +1,20 @@
 main () {
   List<String>students =['Hossain','Diganta','Mostakem','Nafian','Rafiqe','Karim'];
-  List<int>students =[1000,500,'Rana','Rajib','Shakib'];
+  List<int>amount =[1000,500,700,800,900];
+
   print("student 2 index : ${students[2]}");
+
+for(int index=0;index<=students.length; index++){
+  print("student $index name : ${students[index]} . \n your ${amount[index]}  tk payment due");
+
+}
+
+  for(var student in students){
+    int index = students.indexOf(student);
+    print("Student $index name : ${students[index]} \nyour ${amount[index]} payment due");
+  }
+
+  students.asMap().forEach((index, student) {
+    print("Student $index name : ${students[index]} \nyour ${amount[index]} payment due");
+  });
 }
