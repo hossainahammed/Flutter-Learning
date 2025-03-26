16dca8d1e8f7c48a97175ce95d48ebb3647662ba
hossainahammed <hossainahammed627@gmail.com>
2025-03-26 20:17:30 +0600

    try_catch

---
+++ lib/for_loop.dart
@@ -0,0 +1,5 @@
+main () {
+  List<String>students =['Hossain','Diganta','Mostakem','Nafian','Rafiqe','Karim'];
+  List<int>students =[1000,500,'Rana','Rajib','Shakib'];
+  print("student 2 index : ${students[2]}");
+}
