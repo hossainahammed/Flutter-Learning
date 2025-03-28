1fe6dff6c81e552f8d967035de5148788bc0c85f
hossainahammed <hossainahammed627@gmail.com>
2025-03-28 17:05:52 +0600

    OOP Concept Encapsulation_getter

---
+++ lib/OOP_Concept/encapsulation.dart
@@ -0,0 +1,11 @@
+import 'resturent.dart';
+
+main(){
+ Resturant kfc = Resturant();
+
+
+  kfc.order('Chicken Fry');
+  // kfc.prepareItem('Chiken Fry'); -->> prepareItem() methodtike private korar karone amra eita r acces korte partecina. Resturant claass er vitor thaka prepareItem() method er samne theke "_" underscore uthanor maddome amra private mukto korle abr use korte parbo .
+
+ print('Resturant id is ${kfc.resturantId}');//By using getter method we access private variable.
+}
---
+++ lib/OOP_Concept/resturent.dart
@@ -0,0 +1,21 @@
+class Resturant{
+
+  int _id=2023;
+
+  order(String item){
+    print("$item is ordered");
+    _shopping(item);
+    _prepareItem(item);
+    print("$item is served");
+  }
+  _prepareItem(String item){  // Using '_' we make private.Resturant Class er baire keo access korte parbena.
+    print('$item cooking');
+  }
+  _shopping(String item){  // Using '_' we make private.Resturant Class er baire keo access korte parbena.
+    print('Buy material');
+
+  }
+
+  // getter method for access private value of a class instance
+  int get resturantId =>_id;
+}
