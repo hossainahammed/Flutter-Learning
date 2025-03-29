7f24330dfbfeac21b600b7db0c3ec494b03c430c
hossainahammed <hossainahammed627@gmail.com>
2025-03-29 13:32:24 +0600

    polymorphism

--- lib/OOP_Concept/abstract.dart
+++ lib/OOP_Concept/abstract.dart
@@ -1,23 +1,68 @@
-class student{
+abstract class Student{
 
 
-  attendClass(){
-    print('Joined class');
-
-  }
+  attendClass(); //jdi kuno abstract class er method er body na dei tahole ta abstract method hoy.
    examSubmit(){
 
   }
 }
 
-class person extends student{
+class person extends Student{
   String name;
   person(this.name) ;
+
+  @override
+  attendClass() {
+    print("$name joined class");
+  }
 
 
 }
 
+class Businessman implements Student {
+  // implements use korle parrent class er sob methodke forceforly method override korte hbe
+  String name;
+
+  Businessman(this.name);
+
+  @override
+  attendClass() {
+    print("$name Businessman joined class");
+  }
+
+  @override
+  examSubmit() {
+    print(" Businessman $name joined exam");
+  }
+}
 main(){
-  student rafi =student();
+  person rafi =person('Rafi');
   rafi.attendClass();
+
+  Businessman Rahim = Businessman('Rahim');
+  Rahim.examSubmit();
+
+  //polymorphism
+  //student But person(Uni stu,clg,Job holder,job trying), Business
+
+  Student hossain = person('Hossain');
+  Student sabbir = Businessman('Hossain');
+
+  hossain.attendClass();
+  hossain.examSubmit();
+  sabbir.attendClass();
+
+  print(hossain.runtimeType);
+  print(sabbir.runtimeType);
+
+
+  print(hossain is person);
+  print(hossain is Businessman);
+  print(sabbir is person);
+  print(sabbir is Businessman);
+
+
+  print(sabbir is Student);
+  print(hossain is Student);
+
 }
