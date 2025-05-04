522e0220a5063f9021bb07c70123d649f94456a9
Hossain Ahammed <hossainahammed627@gmail.com>
2025-05-04 23:38:27 +0600

    basic code

---
+++ lib/flutter_code/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MyApp ());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'First app',
+      home: Home(),
+
+    );
+  }
+
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
+        centerTitle: true,
+        actions: [
+           
+        ],
+        backgroundColor: Colors.purple,
+      ),
+      body: Text("This is body text",style:TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: Colors.orange,
+
+
+      ),),
+
+    );
+  }
+}
+
