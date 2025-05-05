f242e68e82615f2a7dc7d7f9d7224bd450c135ad
Hossain Ahammed <hossainahammed627@gmail.com>
2025-05-05 23:41:25 +0600

    classs seperate

--- lib/flutter_code/main.dart
+++ lib/flutter_code/main.dart
@@ -1,74 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'My_App.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: 'First app', home: Home());
-  }
-}
 
-class Home extends StatelessWidget {
-  const Home({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My App"),
-        centerTitle: true,
-        actions: [],
-        backgroundColor: Colors.purple,
-      ),
-      body:SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            InkWell(
-              onTap: (){
-
-              },
-                onDoubleTap: (){
-
-                },
-                child: Icon(Icons.add)),
-            IconButton(onPressed:(){
-              print("Item deleted");
-            },
-                icon:Icon(Icons.delete)),
-            TextButton(onPressed: (){
-              print("clicked me from text button");
-            }, child: Text("Click me",style: TextStyle(color: Colors.cyan,fontSize: 30,fontWeight: FontWeight.bold),),),
-            Text("This is row ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.purple),),
-            SizedBox(
-              width: 10,
-            ),
-            Text("This is row ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.purple),),
-            SizedBox(
-              width: 10,
-            ),
-            Text("This is row ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.purple),),
-            SizedBox(
-              width: 10,
-            ),
-            Text("This is row ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.purple),),
-            Text("This is row ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.purple),),
-            Text("This is row ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.purple),),
-            Text("This is row ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.purple),),
-          ],
-        ),
-      )
-
-    );
-  }
-}
