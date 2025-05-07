3408269e3d8351413791a82e439d49b8f3dd3cc4
Hossain Ahammed <hossainahammed627@gmail.com>
2025-05-07 23:39:10 +0600

    Floating action button code

--- lib/flutter_code/Day_2.dart
+++ lib/flutter_code/Day_2.dart
@@ -46,7 +46,7 @@ class Day_2 extends StatelessWidget {
       floatingActionButton: FloatingActionButton(onPressed: (){
         print(Text("Floating action button "));
       },
-        child: Icon(Icons.dangerous),
+        child: Icon(Icons.add),
         backgroundColor:Colors.cyan,
       ),
 
