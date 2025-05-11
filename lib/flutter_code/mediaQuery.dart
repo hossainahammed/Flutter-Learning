8242a3954694f0337aeabee8e1dae9db161fe71f
hossainahammed <hossainahammed627@gmail.com>
2025-05-12 01:50:30 +0600

    Media query all  code

--- lib/flutter_code/mediaQuery.dart
+++ lib/flutter_code/mediaQuery.dart
@@ -2,35 +2,58 @@ import 'package:flutter/material.dart';
 
 class MediaQury extends StatefulWidget {
   const MediaQury({super.key});
-
   @override
   State<MediaQury> createState() => _MediaQuryState();
 }
-
 class _MediaQuryState extends State<MediaQury> {
 
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
+    // double screenWidth = MediaQuery.of(context).size.width;
+    double fontSize = screenSize.width > 600 ? 32 : 18;
+    double pexelRatio = MediaQuery.of(context).devicePixelRatio;
     return Scaffold(
-      body: Container(
-        width: screenSize.width,
-        height: screenSize.height,
-        color: Colors.orangeAccent,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Container(
-                width: screenSize.width*0.2,
-                height: screenSize.height*0.3,
-                color: Colors.red,
-              ),
-            )
-          ],
-        ),
-      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              width: screenSize.width*0.5,
+              height: screenSize.height*0.3,
+              color: Colors.red,
+            ),
+          ),
 
+          Text('This is Media Query text',
+            style: TextStyle(
+                fontSize: fontSize
+            ),
+          ),
+          Text('pixel Ratio : $pexelRatio'),
+
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 4.0,
+            children: List.generate(50, (index) {
+              return Container(
+                color: Colors.red,
+                height: 50,
+                width: 50,
+              );
+            }),
+
+          ),
+
+          // LayoutBuilder(builder: (context,constraints){
+          //   if(constraints.maxWidth > 600){
+          //     return GridView.count(crossAxisCount: 5);
+          //   }else{
+          //     return GridView.count(crossAxisCount: 1);
+          //   }
+          // })
+        ],
+      ),
     );
   }
 }
