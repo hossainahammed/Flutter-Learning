6999d4f7aa0a43d36fcf68cecd8c5aabe72c4b1f
hossainahammed <hossainahammed627@gmail.com>
2025-05-13 01:43:25 +0600

    Aspect ratio Expended  code

--- lib/flutter_code/aspectRatio.dart
+++ lib/flutter_code/aspectRatio.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class aspect_ratio extends StatefulWidget {
   const aspect_ratio({super.key});
 
@@ -10,6 +10,130 @@ class aspect_ratio extends StatefulWidget {
 class _aspect_ratioState extends State<aspect_ratio> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Module 9 class 2'),
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+
+            Container(
+              height: 200.h,
+              width: 300.w,
+              color: Colors.blueGrey,
+            ),
+
+            Text('This is responsive text',style: TextStyle(
+
+                fontSize: 30.sp
+            ),),
+
+            FractionallySizedBox(
+              widthFactor: 0.5,
+              child: ElevatedButton(
+                onPressed: (){},
+                child: Text('Submit'),
+              ),
+            ),
+
+
+            Container(
+
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black,width: 2)
+              ),
+
+              height: 300,
+              child: FractionallySizedBox(
+                heightFactor: 0.5,
+                child: Container(
+                  color: Colors.blue,
+                  child: Center(child: Text('This is fg')),
+                ),
+              ),
+            ),
+
+            Row(
+              children: [
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ),
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    height: 100,
+                    color: Colors.redAccent,
+                  ),
+                )
+              ],
+            ),
+
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  color: Colors.deepPurpleAccent,
+                ),
+                Container(
+                  height: 50,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+
+
+
+            Row(
+              children: [
+                Expanded(
+                    flex: 2,
+                    child: ElevatedButton(onPressed: (){}, child: Text('person-1'))),
+                Expanded(child: ElevatedButton(onPressed: (){}, child: Text('person-2'))),
+                Expanded(child: ElevatedButton(onPressed: (){}, child: Text('person-3'))),
+              ],
+            ),
+
+
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(
+                color: Colors.orange,
+              ),
+            ),
+
+            AspectRatio(
+                aspectRatio: 9 / 16,
+                child: Container(
+                  color: Colors.red,
+                )
+            ),
+
+
+            AspectRatio(
+                aspectRatio: 16 / 10,
+                child: Container(
+                  color: Colors.green,
+                )
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/flutter_code/aspect_ratio main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'aspectRatio.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_ , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          home: child,
+        );
+      },
+      child: const aspect_ratio (),
+    );
+  }
+
+}
