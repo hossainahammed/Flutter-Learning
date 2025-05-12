c171317569904178e2122d3a2625d78b5afd1df0
hossainahammed <hossainahammed627@gmail.com>
2025-05-13 00:19:23 +0600

    code

---
+++ lib/flutter_code/aspectRatio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class aspect_ratio extends StatefulWidget {
+  const aspect_ratio({super.key});
+
+  @override
+  State<aspect_ratio> createState() => _aspect_ratioState();
+}
+
+class _aspect_ratioState extends State<aspect_ratio> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
