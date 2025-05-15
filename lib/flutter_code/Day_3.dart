d7a048d097c2b3ffde5c74a9026e9b09bf4b0ac2
hossainahammed <hossainahammed627@gmail.com>
2025-05-15 14:24:50 +0600

    Tab controller

--- lib/flutter_code/Day_3.dart
+++ lib/flutter_code/Day_3.dart
@@ -10,72 +10,80 @@ class Day_3 extends StatefulWidget {
 class _Day_3State extends State<Day_3> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('This is extra widget'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(child: Column(
-              children: [
-                CircleAvatar(
-                  radius :35,
-                  backgroundImage: NetworkImage('url'),
-                ),
-                SizedBox(
-                  height: 12,
-                ),
-                Text('Hossain Ahammed',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,fontSize: 18),),
-                Text('hossain@gmail.com',style: TextStyle(fontSize: 14),)
-              ],
-            )),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('This is extra widget'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(child: Column(
+                children: [
+                  CircleAvatar(
+                    radius :35,
+                    backgroundImage: NetworkImage('url'),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text('Hossain Ahammed',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,fontSize: 18),),
+                  Text('hossain@gmail.com',style: TextStyle(fontSize: 14),)
+                ],
+              )),
 
-            ListTile(
-              title: Text('Home'),
-              onTap: (){},
-            ),
+              ListTile(
+                title: Text('Home'),
+                onTap: (){},
+              ),
 
-            Divider(
-              thickness: 1,
-              color: Colors.blueGrey,
-            ),
-
-
-            ListTile(
-              title: Text('Item-1'),
-              onTap: (){},
-            ),
-
-            Divider(
-              thickness: 1,
-              color: Colors.blueGrey,
-            ),
+              Divider(
+                thickness: 1,
+                color: Colors.blueGrey,
+              ),
 
 
-            ListTile(
-              title: Text('Item-2'),
-              onTap: (){},
-            ),
-            Divider(
-              thickness: 1,
-              color: Colors.blueGrey,
-            ),
+              ListTile(
+                title: Text('Item-1'),
+                onTap: (){},
+              ),
+
+              Divider(
+                thickness: 1,
+                color: Colors.blueGrey,
+              ),
+
+
+              ListTile(
+                title: Text('Item-2'),
+                onTap: (){},
+              ),
+              Divider(
+                thickness: 1,
+                color: Colors.blueGrey,
+              ),
 
 
 
-            ListTile(
-              title: Text('Item-3'),
-              onTap: (){},
-            ),
-          ],
+              ListTile(
+                title: Text('Item-3'),
+                onTap: (){},
+              ),
+              Divider(
+                thickness: 1,
+                color: Colors.blueGrey,
+              ),
+
+            ],
+          ),
+
+
         ),
 
-
       ),
-
     );
   }
 }
