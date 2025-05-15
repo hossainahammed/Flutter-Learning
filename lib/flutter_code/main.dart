d8a97d02d8f5fcec101fb1a16a04f718e8e735b6
hossainahammed <hossainahammed627@gmail.com>
2025-05-15 13:14:53 +0600

    by using package Device preview

--- lib/flutter_code/main.dart
+++ lib/flutter_code/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:if_else_statement/flutter_code/aspect_ratio%20main.dart';
 import 'My_App.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(MyApp1());
+
+  runApp(
+      DevicePreview(
+        enabled: true,
+        builder: (context)=>MyApp1() ,
+      )
+  );
+
 }
 
 
