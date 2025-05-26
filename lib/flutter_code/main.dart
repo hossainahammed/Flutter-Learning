cdb87847a5628facb18c2adc621ce6581fdce9b4
hossainahammed <hossainahammed627@gmail.com>
2025-05-26 22:48:35 +0600

    Water tracker app initialize

--- lib/flutter_code/main.dart
+++ lib/flutter_code/main.dart
@@ -3,13 +3,16 @@ import 'package:if_else_statement/flutter_code/aspect_ratio%20main.dart';
 import 'My_App.dart';
 import 'package:device_preview/device_preview.dart';
 
+import 'waterTracker.dart';
+
 void main() {
 
   runApp(
       DevicePreview(
         enabled: false ,
         //builder: (context)=>MyApp1() ,
-        builder: (context)=>MyApp() ,
+        //builder: (context)=>MyApp() ,
+        builder: (context)=>waterTracker() ,
       )
   );
 
---
+++ lib/flutter_code/waterTracker.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class waterTracker extends StatefulWidget {
+  const waterTracker({super.key});
+
+  @override
+  State<waterTracker> createState() => _waterTrackerState();
+}
+
+class _waterTrackerState extends State<waterTracker> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
