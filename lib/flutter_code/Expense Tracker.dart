48fcc78713b5064c7d8e50f92eb9c840aa952946
hossainahammed <hossainahammed627@gmail.com>
2025-05-28 22:39:17 +0600

    Expense tracker App initialize

---
+++ lib/flutter_code/Expense Tracker.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Expense_Tracker extends StatefulWidget {
+  const Expense_Tracker({super.key});
+
+  @override
+  State<Expense_Tracker> createState() => _Expense_TrackerState();
+}
+
+class _Expense_TrackerState extends State<Expense_Tracker> {
+
+  final List<String> categories=[
+    'Food',
+    'Transport',
+    'Enter',
+    'Bills'
+  ];
+
+  double totall = 0.0;
+
+  void _showForm(){
+    String selectedCategory  = categories.first;
+    showModalBottomSheet(context: context, builder:(context){
+      return Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          right: 16,
+          top: 16,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Amount'
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            DropdownButtonFormField(items: categories.map((category) => DropdownMenuItem(value: category,child: Text(category),)).toList(),
+                onChanged: (value) => selectedCategory = value!)
+
+          ],
+        ),
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Tracker"),
+        actions: [
+          //IconButton(onPressed: ()=>_showForm(), icon: icon)
+        ],
+      ),
+    );
+  }
+}
--- lib/flutter_code/My_App.dart
+++ lib/flutter_code/My_App.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:if_else_statement/flutter_code/stateful_class.dart';
 import '../todo.dart';
 import 'Day_2.dart';
+import 'Expense Tracker.dart';
 import 'Home.dart';
 import 'calculatorApp.dart';
 import 'form_login.dart';
@@ -23,7 +24,8 @@ class MyApp extends StatelessWidget {
         // home: TodoApp());
         //home:MediaQury());
          //home:CalculatorApp());
-         home:waterTracker());
+         //home:waterTracker());
+         home:Expense_Tracker());
 
 
 
