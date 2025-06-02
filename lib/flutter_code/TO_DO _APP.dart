e2ba6b6cb7fa9ae925453ed3561fb9f105d5fde2
hossainahammed <hossainahammed627@gmail.com>
2025-06-02 18:29:53 +0600

    Implement two card for active and complete

--- lib/flutter_code/TO_DO _APP.dart
+++ lib/flutter_code/TO_DO _APP.dart
@@ -10,6 +10,86 @@ class TdoScreen extends StatefulWidget {
 class _TdoScreenState extends State<TdoScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      appBar: AppBar(
+        title: Text('Todo app'),
+      ),
+
+      body: Column(
+        children: [
+          SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+
+              Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4,
+                      )
+                    ]
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: [
+                        Text('10',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                        Text('Active', style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.w500,
+                        ),),
+                      ],
+                
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 4,
+                        )
+                      ]
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: [
+                        Text('10',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        Text('Completed', style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.w500,
+                        ),),
+                      ],
+
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+
+        ],
+      ),
+    );
   }
 }
