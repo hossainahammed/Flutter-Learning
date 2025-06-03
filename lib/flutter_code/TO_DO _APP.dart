80c64b331b24cb04a5806a180a11f4733b9d52fa
hossainahammed <hossainahammed627@gmail.com>
2025-06-03 20:53:31 +0600

    Implement linethrough code for deleted task

--- lib/flutter_code/TO_DO _APP.dart
+++ lib/flutter_code/TO_DO _APP.dart
@@ -8,85 +8,80 @@ class TdoScreen extends StatefulWidget {
 }
 
 class _TdoScreenState extends State<TdoScreen> {
-
-  List<Map<String,dynamic>> tasks =[];
+  List<Map<String, dynamic>> tasks = [];
   bool showActiveTask = true;
 
-  void _addTask(String task){
+  void _addTask(String task) {
     setState(() {
-      tasks.add(
-        {
-          'task' :task,
-          'completed' : false,
-        }
-
-      );
+      tasks.add({'task': task, 'completed': false});
       Navigator.pop(context);
     });
   }
-  
 
-  void _showTaskDialouge({int ? index}){
+  void _showTaskDialouge({int? index}) {
     TextEditingController _taskController = TextEditingController();
 
-    showDialog(context: context, builder: (context)=>AlertDialog(
-      title: Text('Add Task'),
-      content: TextField(
-        controller: _taskController,
-        decoration: InputDecoration(
-          hintText: 'Enter Task'
-        ),
-      ),
-      actions: [
-        TextButton(onPressed: (){
-          Navigator.pop(context);
-        }, child: Text('Cancel')),
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Text('Add Task'),
+            content: TextField(
+              controller: _taskController,
+              decoration: InputDecoration(hintText: 'Enter Task'),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              ),
 
-        ElevatedButton(style:ElevatedButton.styleFrom(
-        shape: StadiumBorder()
-        ),onPressed: (){
-          if(_taskController.text.trim().isNotEmpty){
-            _addTask(_taskController.text);
-          }
-        }, child: Text('Save')),
-      ],
-
-    ));
-
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+                onPressed: () {
+                  if (_taskController.text.trim().isNotEmpty) {
+                    _addTask(_taskController.text);
+                  }
+                },
+                child: Text('Save'),
+              ),
+            ],
+          ),
+    );
   }
-  void toggleTaskStatus(int index){
+
+  void toggleTaskStatus(int index) {
     setState(() {
-      tasks[index]['completed'] =!tasks[index]['completed'];
+      tasks[index]['completed'] = !tasks[index]['completed'];
     });
-
   }
 
-  void deleteTask(int index){
+  void deleteTask(int index) {
     setState(() {
       tasks.removeAt(index);
-
     });
   }
-  int get activeCount => tasks.where((task)=>!task['completed']).length;
-  int get completedCount => tasks.where((task)=>task['completed']).length;
+
+  int get activeCount => tasks.where((task) => !task['completed']).length;
+  int get completedCount => tasks.where((task) => task['completed']).length;
   @override
   Widget build(BuildContext context) {
-    List<Map<String,dynamic>> filterdTasks = tasks.where((task)=>task['completed'] !=showActiveTask).toList();
+    List<Map<String, dynamic>> filterdTasks =
+        tasks.where((task) => task['completed'] != showActiveTask).toList();
     return Scaffold(
       backgroundColor: Colors.blue.shade50,
-      appBar: AppBar(
-        title: Text('Todo app'),
-      ),
+      appBar: AppBar(title: Text('Todo app')),
 
       body: Column(
         children: [
-          SizedBox(height: 10,),
+          SizedBox(height: 10),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-
               InkWell(
-                onTap: (){
+                onTap: () {
                   setState(() {
                     showActiveTask = true;
                   });
@@ -97,113 +92,122 @@ class _TdoScreenState extends State<TdoScreen> {
                       color: Colors.orange,
                       borderRadius: BorderRadius.circular(10),
                       boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 4,
-                        )
-                      ]
+                        BoxShadow(color: Colors.black12, blurRadius: 4),
+                      ],
                     ),
                     child: Padding(
                       padding: const EdgeInsets.all(18.0),
                       child: Column(
                         children: [
-                          Text(activeCount.toString(),
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),),
-                          Text('Active', style: TextStyle(
-                            fontSize: 23,
-                            fontWeight: FontWeight.w500,
-                          ),),
+                          Text(
+                            activeCount.toString(),
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Active',
+                            style: TextStyle(
+                              fontSize: 23,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
                         ],
-
                       ),
                     ),
                   ),
                 ),
               ),
               InkWell(
-                onTap: (){
+                onTap: () {
                   setState(() {
-                    showActiveTask= false;
+                    showActiveTask = false;
                   });
                 },
                 child: Card(
                   child: Container(
                     decoration: BoxDecoration(
-                        color: Colors.green,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 4,
-                          )
-                        ]
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(color: Colors.black12, blurRadius: 4),
+                      ],
                     ),
                     child: Padding(
                       padding: const EdgeInsets.all(18.0),
                       child: Column(
                         children: [
-                          Text(completedCount.toString(),
+                          Text(
+                            completedCount.toString(),
                             style: TextStyle(
                               fontSize: 25,
                               fontWeight: FontWeight.bold,
-                            ),),
-                          Text('Completed', style: TextStyle(
-                            fontSize: 23,
-                            fontWeight: FontWeight.w500,
-                          ),),
+                            ),
+                          ),
+                          Text(
+                            'Completed',
+                            style: TextStyle(
+                              fontSize: 23,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
                         ],
-
                       ),
                     ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
           Expanded(
             child: ListView.builder(
-                itemCount: filterdTasks.length,
-                itemBuilder: (context,index){
-                  return Dismissible(
-                    key: Key(UniqueKey().toString()),
-                    background: Container(
-                      color: Colors.green,
-                      child: Icon(Icons.check_box,color:Colors.white,),
-                      
-                    ),
-                    
-                    secondaryBackground: Container(
-                      color: Colors.red,
-                      child: Icon(Icons.delete,color: Colors.white,),
-                    ),
-                    onDismissed: (direction){
-                      if(direction == DismissDirection.startToEnd){
-                        // Task complete
-                        toggleTaskStatus(index);
-                      }
-                      else{
-                        //delete task
-                        deleteTask(index);
-                      }
-                    },
-                    child: Card(
-                      child: ListTile(
-                        title: Text(filterdTasks[index]['task']),
+              itemCount: filterdTasks.length,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  key: Key(UniqueKey().toString()),
+                  background: Container(
+                    color: Colors.green,
+                    child: Icon(Icons.check_box, color: Colors.white),
+                  ),
+
+                  secondaryBackground: Container(
+                    color: Colors.red,
+                    child: Icon(Icons.delete, color: Colors.white),
+                  ),
+                  onDismissed: (direction) {
+                    if (direction == DismissDirection.startToEnd) {
+                      // Task complete
+                      toggleTaskStatus(index);
+                    } else {
+                      //delete task
+                      deleteTask(index);
+                    }
+                  },
+                  child: Card(
+                    child: ListTile(
+                      title: Text(
+                        filterdTasks[index]['task'],
+                        style: TextStyle(
+                          fontSize: 16,
+                          decoration:
+                              filterdTasks[index]['completed']
+                                  ? TextDecoration.lineThrough
+                                  : null,
+                        ),
                       ),
-
                     ),
-                  );
-            }),
-          )
-
-
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: ()=>_showTaskDialouge(),
-        child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showTaskDialouge(),
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
