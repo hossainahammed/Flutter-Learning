d18ae3f7154f3ad47eb7a5e4a1e2b7ed8ad51e44
hossainahammed <hossainahammed627@gmail.com>
2025-06-24 23:55:03 +0600

    Do delete icon work code

--- lib/flutter_code/API_WOrk.dart
+++ lib/flutter_code/API_WOrk.dart
@@ -101,7 +101,7 @@ class _ApiWorkState extends State<ApiWork> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product Cruud'),
+        title: Text('Product Crud'),
         backgroundColor: Colors.orange,
         centerTitle: true,
       ),
