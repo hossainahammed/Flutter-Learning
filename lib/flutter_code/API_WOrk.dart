a9dace887eefc5fde309bfecca1979cadfe89d5e
hossainahammed <hossainahammed627@gmail.com>
2025-06-27 23:26:44 +0600

    update add product

--- lib/flutter_code/API_WOrk.dart
+++ lib/flutter_code/API_WOrk.dart
@@ -201,7 +201,7 @@ class _ApiWorkState extends State<ApiWork> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product Crud'),
+        title: Text('Product Crudd'),
         backgroundColor: Colors.orange,
         centerTitle: true,
       ),
