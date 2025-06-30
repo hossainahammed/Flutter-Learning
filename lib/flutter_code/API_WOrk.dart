401092b578da5f0d3dd2c1c75f2312b4ad3b2e8c
hossainahammed <hossainahammed627@gmail.com>
2025-06-30 23:15:11 +0600

    update add product

--- lib/flutter_code/API_WOrk.dart
+++ lib/flutter_code/API_WOrk.dart
@@ -207,7 +207,7 @@ class _ApiWorkState extends State<ApiWork> {
         centerTitle: true,
       ),
       body: productcontroller.products.isEmpty
-          ? Center(child: Text('No products available'))
+          ? Center(child: Text('No products availablee'))
           : GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
